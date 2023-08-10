855ab84d60fbef457eed7e566b40527954bfe352
redhawk603 <kapil.raghul@gmail.com>
2023-08-10 13:41:58 +0530

    backend api added globally

--- test1/index.js
+++ test1/index.js
@@ -38,7 +38,7 @@ async function submit() {
     
         try {
             const response = await 
-                fetch("http://localhost:3000/userData",  {
+                fetch("https://test-backend-q.onrender.com/userData",  {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
