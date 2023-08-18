ad4b2c2bc532683a5ab9a32d8cdbf0c8d5b57682
redhawk603 <kapil.raghul@gmail.com>
2023-08-18 16:20:21 +0530

    view count added

--- test1/index.js
+++ test1/index.js
@@ -58,7 +58,7 @@ async function submit() {
             alert(result.msg)
             alert("Hello " + name + "! " + "Your results will be sent to " + email + ".");
 
-            window.location.href = "index2.html?name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email) + "&_id=" + encodeURIComponent(userId);
+            window.location.href = "index2.html?name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email) + "&_id=" + encodeURIComponent(userId) ;
             // document.getElementById("username").value = name;
             // document.getElementById("useremail").value = email;
 
@@ -128,7 +128,10 @@ async function submit2() {
 
     // Calculate the total score based on the selected answers
     var anSum = parseInt(answer1) + parseInt(answer2) + parseInt(answer3) + parseInt(answer4) + parseInt(answer5);
-
+    var GryffindorViews = 0
+    var HufflepuffViews = 0
+    var RavenclawViews = 0
+    var SlytherinViews = 0
     var aResult = "Muggle";
     var reDesc = "normal person. Please answer the questions";
     if (answer1 === "0" || answer2 === "0" || answer3 === "0" || answer4 === "0" || answer5 === "0") {
@@ -138,21 +141,25 @@ async function submit2() {
         aResult = "Hufflepuff";
         reDesc = "loyal and friendly person! People enjoy being around you, and you can always put a smile on someone's face. You are a hard worker and are always willing to help others. You are a great friend to have";
         alert("Your results will be sent to your email by clicking the button below!");
+        var HufflepuffViews = HufflepuffViews + 1
     }
     else if (anSum === 90 || anSum === 100 || anSum === 110 || anSum === 120) {
         aResult = "Ravenclaw";
         reDesc = "intelligent and creative person! You are always looking for new ways to learn and grow. You are a great problem solver and are always willing to help others. You are very intelligent"
         alert("Your results will be sent to your email by clicking the button below!");
+        var RavenclawViews = RavenclawViews + 1
     }
     else if (anSum === 130 || anSum === 140 || anSum === 150 || anSum === 160) {
         aResult = "Gryffindor";
         reDesc = "brave and courageous person! You are always willing to stand up for what you believe in and are not afraid to take risks. You are a great leader and are always willing to help others. You are very brave"
         alert("Your results will be sent to your email by clicking the button below!");
+        var GryffindorViews = GryffindorViews + 1
     }
     else if (anSum === 170 || anSum === 180 || anSum === 190 || anSum === 200) {
         aResult = "Slytherin";
         reDesc = "ambitious and cunning person! You are always looking for new ways to get ahead and are not afraid to take risks. You are a great leader and are always willing to help others. You are very ambitious";
         alert("Your results will be sent to your email by clicking the button below!");
+        var SlytherinViews = SlytherinViews + 1;
 
     }
     else {
@@ -161,6 +168,7 @@ async function submit2() {
 
     anResult = aResult;
     resultDesc = reDesc;
+    console.log(GryffindorViews, HufflepuffViews, RavenclawViews, SlytherinViews,"submit 2")
     console.log(resultDesc)
     document.getElementById("page2Btn").innerHTML = ""
     let btn = document.getElementById("page2Btn")
@@ -174,13 +182,26 @@ async function submit2() {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    result: anResult
+                    result: anResult,
+                    views: {
+                        GryffindorViews: GryffindorViews,
+                        HufflepuffViews: HufflepuffViews,
+                        RavenclawViews: RavenclawViews,
+                        SlytherinViews: SlytherinViews
+                    }
+
                 }),
             })
         const result = await response.json();
         console.log(result)
         btn.classList.remove("loading")
         document.getElementById("page2Btn").innerHTML = "Submit"
+        // emi(userId, username, useremail, GryffindorViews, HufflepuffViews, RavenclawViews, SlytherinViews)
+        // window.location.href = "index2.html?name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email) + "&_id=" + encodeURIComponent(userId) + "?GryffindorViews=" + encodeURIComponent(GryffindorViews) + "&HufflepuffViews=" + encodeURIComponent(HufflepuffViews) + "&RavenclawViews=" + encodeURIComponent(RavenclawViews) + "&SlytherinViews=" + encodeURIComponent(SlytherinViews) ;
+       console.log(112233)
+        await viewData()
+        history.pushState({}, null, `index2.html?name=${username}&email=${useremail}&_id=${userId}&GryffindorViews=${GryffindorViews}&HufflepuffViews=${HufflepuffViews}&RavenclawViews=${RavenclawViews}&SlytherinViews=${SlytherinViews}`);
+
     }
 
     catch (err) {
@@ -194,12 +215,18 @@ async function submit2() {
 
 }
 
+var check = GryffindorViews, HufflepuffViews, RavenclawViews, SlytherinViews
 
 function emi(to_name, to_email, anResult, resultDesc) {
+    var viewParams = new URLSearchParams(window.location.search);
+    let GryffindorViews = viewParams.get('GryffindorViews');
+    let HufflepuffViews = viewParams.get('HufflepuffViews');
+    let RavenclawViews = viewParams.get('RavenclawViews');
+    let SlytherinViews = viewParams.get('SlytherinViews');
     document.getElementById("page2Btn").innerHTML = ""
     let btn = document.getElementById("page2Btn")
     btn.classList.add("loading")
-
+    console.log(GryffindorViews, HufflepuffViews, RavenclawViews, SlytherinViews)
     console.log(resultDesc)
     var templateParams = {
         tonam: to_name,
@@ -220,11 +247,9 @@ function emi(to_name, to_email, anResult, resultDesc) {
         });
 
     alert("Your results have been sent to your email!");
-    window.location.href = "index4.html";
+    window.location.href = "index4.html" + "?GryffindorViews=" + encodeURIComponent(GryffindorViews) + "&HufflepuffViews=" + encodeURIComponent(HufflepuffViews) + "&RavenclawViews=" + encodeURIComponent(RavenclawViews) + "&SlytherinViews=" + encodeURIComponent(SlytherinViews);
     btn.classList.remove("loading")
     document.getElementById("page2Btn").innerHTML = "Submit"
-
-
 };
 
 async function submit3() {
@@ -290,4 +315,88 @@ function back() {
     userId = new URL(window.location.href).searchParams.get("_id");
     window.location.href = "index3.html" + "?_id=" + encodeURIComponent(userId);
 
+}
+async function viewData() {
+
+    console.log(889977)
+
+
+    var viewParams = new URLSearchParams(window.location.search);
+    let GryffindorViews = viewParams.get('GryffindorViews');
+    let RavenclawViews = viewParams.get('RavenclawViews');
+    let HufflepuffViews = viewParams.get('HufflepuffViews');
+    let SlytherinViews = viewParams.get('SlytherinViews');
+    
+
+    try {
+       
+        const response = await
+            fetch("https://test-backend-q.onrender.com/views/update", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    GryffindorViews: Number(GryffindorViews),
+                    HufflepuffViews: Number(HufflepuffViews),
+                    RavenclawViews: Number(RavenclawViews),
+                    SlytherinViews:Number (SlytherinViews)
+
+                }),
+            })
+        const result = await response.json();
+        console.log(result)
+    }
+    catch (err) {
+       console.log(err)
+    }
+    console.log(4567899000)
+  
+} 
+
+
+// try {
+//     const response = await
+//     fetch(`https://test-backend-q.onrender.com/views`, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+            
+//                     GryffindorViews: GryffindorViews,
+//                     // HufflepuffViews: HufflepuffViews,
+//                     // RavenclawViews: RavenclawViews,
+//                     // SlytherinViews: SlytherinViews
+//             }),
+//         })
+//         const result = await response.json();
+
+//         console.log(result)
+//     }
+
+//     catch(err) {
+//         console.log(err)
+//     }
+
+function getV() {
+    fetch("https://test-backend-q.onrender.com/views").then(res=>res.json()).then(data=>
+    {
+
+        const {
+
+            GryffindorViews,
+    RavenclawViews,
+    HufflepuffViews,
+    SlytherinViews,
+        } = data
+    document.getElementById("GryffindorViews").innerHTML= GryffindorViews
+    document.getElementById("RavenclawViews").innerHTML= RavenclawViews
+    document.getElementById("HufflepuffViews").innerHTML= HufflepuffViews
+    document.getElementById("SlytherinViews").innerHTML= SlytherinViews
+    console.log(4456677)
+
+    }
+    
+    ).catch(err=> alert('There was an error! Please try again!'))
 }
